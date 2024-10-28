d9511e6fae5a39a1a2dd781660f8dbe1a930acd2
uchihaashar <mohdasharshakeel@gmail.com>
2024-10-28 11:01:40 +0530

    connect to mongo atles

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async()=>{
+try {
+  await mongoose.connect(`${process.env.MONGODB_URL}/${advanedyoutube}`)
+} catch (error) {
+    console.error("ERROR ",error);
+    throw error
+    
+}
+})()
+*/
