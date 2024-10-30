affa06576c8a5f749b7f118d15fa4d74cadcaa6f
uchihaashar <mohdasharshakeel@gmail.com>
2024-10-30 11:28:38 +0530

    custom Api ( utils code )

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(()=>{
+  app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`listening on ${process.env.PORT}`);
+    
+  })
+})
+.catch((err)=>{
+console.log("MONGODB CONNECTION FAILED: " + err);
+})
 
 
 
